17bb618e0d241243a9ffcc3590b6e5fc9cdacd3b
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-05-03 12:40:36 +0300

    Add  NavBar

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,22 @@
+import { type ComponentProps } from "react";
+import { NavLink } from "react-router";
+import styles from "./NavBar.module.css";
+import { FaList, FaInfo } from "react-icons/fa";
+
+// FIXME: mobile - after click close sidebar
+function NavBar({ className, ...props }: ComponentProps<"nav">) {
+  return (
+    <nav {...props} className={[styles.nav, className].join(" ")}>
+      <NavLink to="/">
+        <FaList />
+        <span>Todo</span>
+      </NavLink>
+      <NavLink to="/about">
+        <FaInfo />
+        <span>About</span>
+      </NavLink>
+    </nav>
+  );
+}
+
+export default NavBar;
