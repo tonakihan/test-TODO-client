0de2abfe95b5ce055074b7625e46ff50ed9d0b18
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-05-15 20:48:36 +0300

    Add config

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_URL } from "../config/api";
 
 const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://jsonplaceholder.typicode.com/",
+    baseUrl: API_URL,
   }),
   tagTypes: ["Todos"],
   endpoints: () => ({}),
