44b5dd313ebe315647ada72ea410b7f3aa64e655
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-05-16 19:53:49 +0300

    Fix base url in react router for deployment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,35 +5,40 @@ import AboutPage from "../pages/AboutPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import TodoItemPage from "../pages/TodoItemPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: App,
+      children: [
+        {
+          index: true,
+          loader: () => redirect("/todo"),
+        },
+        {
+          path: "/todo",
+          Component: TodoPage,
+          children: [
+            {
+              path: ":id",
+              Component: TodoItemPage,
+            },
+          ],
+        },
+        {
+          path: "/about",
+          Component: AboutPage,
+        },
+        {
+          path: "*",
+          Component: NotFoundPage,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    Component: App,
-    children: [
-      {
-        index: true,
-        loader: () => redirect("/todo"),
-      },
-      {
-        path: "/todo",
-        Component: TodoPage,
-        children: [
-          {
-            path: ":id",
-            Component: TodoItemPage,
-          },
-        ],
-      },
-      {
-        path: "/about",
-        Component: AboutPage,
-      },
-      {
-        path: "*",
-        Component: NotFoundPage,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-]);
+);
 
 export default router;
