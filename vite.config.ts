d2baaacd48855394c4d9d74ec2cf55fdb06a5385
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-05-16 19:42:15 +0300

    Fix again

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.GITHUB_REPOSITORY ?? "/",
+  base: process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "/",
 });
