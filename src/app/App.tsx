d515b8549f4909621d5bb913c9d1ad3aad90a90b
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-05-20 20:13:10 +0300

    Fix import components after relocation Header & Footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@mui/material/styles";
-import Footer from "../components/layout/Footer";
-import Header from "../components/layout/Header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import styles from "./app.module.css";
 import { Outlet } from "react-router";
 import { appTheme } from "../styles/theme";
