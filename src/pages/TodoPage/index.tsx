a20525d83598cce9f2efc7d57fcd59370ac442e6
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-05-21 21:41:04 +0300

    Update TodoPage: Add edit items, Add pagination

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,51 +1,103 @@
 import { useEffect } from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
+//
+import DefaultLayout from "../../components/layout/DefaultLayout";
 //
 import TodoCard from "../../components/TodoCard";
 import ErrorMsg from "../../components/ui/ErrorMsg";
 import Spinner from "../../components/ui/Spinner";
+import Pagination from "../../components/ui/Pagination";
+import Card from "../../components/ui/Card";
 //
 import { useAppDispath, useAppSelector } from "../../hooks/store";
+import usePagination from "../../hooks/usePagination";
 import { fetchTodos } from "../../features/todos/services/thunks";
 import {
   selectTodoIds,
   selectTodosStatus,
+  selectTodosTotal,
 } from "../../features/todos/store/selectors";
 //
 import styles from "./TodoPage.module.css";
+import { FaPlus } from "react-icons/fa";
+import clsx from "clsx";
 
 const TodoPage = () => {
+  const navigate = useNavigate();
+  //
   const dispatch = useAppDispath();
+  //
   const todoIds = useAppSelector(selectTodoIds);
   const todosStatus = useAppSelector(selectTodosStatus);
+  const todosTotal = useAppSelector(selectTodosTotal);
+  //
+  const countItemsPerPage = 11;
+  const pagination = usePagination(todoIds, countItemsPerPage);
+  const { currentPage, setPage, currentPageItems, totalPages } = pagination;
+
+  function handlerAddTodo() {
+    //TODO Создать temp item и проверить, заполнен ли он.
+  }
 
   useEffect(() => {
-    if (todosStatus === "idle") dispatch(fetchTodos(1));
+    if (todosStatus === "idle") dispatch(fetchTodos({ count: 50 }));
   }, [todosStatus, dispatch]);
+
+  // Loading more data
+  if (totalPages - currentPage < 2) {
+    if (todoIds.at(-1) >= todosTotal)
+      console.log("The data is ended on the server");
+    else if (todosStatus === "succeeded") {
+      dispatch(
+        fetchTodos({
+          count: countItemsPerPage * 2,
+          startIndex: todoIds.at(-1),
+        }),
+      );
+    }
+  }
 
   if (todosStatus === "failed") {
     return <ErrorMsg />;
   }
 
-  if (todosStatus === "loading") {
+  if (todosStatus === "loading" && todoIds.length <= 0) {
     return <Spinner color="info" />;
   }
 
   return (
     <>
-      <h1 className={styles.title}>Todo</h1>
+      <DefaultLayout>
+        <DefaultLayout.Header className={styles.header}>
+          <h1>Todo</h1>
+        </DefaultLayout.Header>
+        {currentPageItems.length === 0 && (
+          <DefaultLayout.Body>
+            <p style={{ textAlign: "center" }}>Список задач пуст</p>
+          </DefaultLayout.Body>
+        )}
 
-      {todoIds.length === 0 && (
-        <p style={{ textAlign: "center" }}>Список задач пуст</p>
-      )}
+        <DefaultLayout.Body className={styles.grid}>
+          {currentPageItems.map((id) => (
+            <TodoCard
+              key={id}
+              todoId={id}
+              onClick={() => navigate(`${id}`)}
+              className={styles.card}
+            />
+          ))}
 
-      <div className={styles.grid}>
-        {todoIds.map((id) => (
-          <TodoCard key={id} todoId={id} className={styles.card} />
-        ))}
-      </div>
-
-      {/* TODO ButtonAdd */}
+          <Card
+            onClick={handlerAddTodo}
+            className={clsx(styles.card, styles.cardAdd)}
+          >
+            <FaPlus size={"2.5em"} />
+          </Card>
+        </DefaultLayout.Body>
+        <DefaultLayout.Footer>
+          <Pagination pagination={pagination} />
+        </DefaultLayout.Footer>
+      </DefaultLayout>
 
       {/* TodoItem - url :id */}
       <Outlet />
