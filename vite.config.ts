e77ade345dea6629053f327453e12f4d3419e4b4
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-05-28 21:48:41 +0300

    Add lib: vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "/",
@@ -11,5 +11,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
 });
